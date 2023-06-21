23110e9991887e1ae64754ab08ff25e4c8772bf7
Ольга Скарговская <119108613+Olgascar@users.noreply.github.com>
2023-06-21 18:45:33 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_BINDS'] = {
-    'messages': 'sqlite:///message.db'
-}
-
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 db = SQLAlchemy(app)
-
+with app.app_context():
+    db.create_all()
 
 
 class Article(db.Model):
@@ -20,20 +18,7 @@ class Article(db.Model):
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Article %r>' %self.id
-
-class Message(db.Model):
-    __bind_key__ = 'messages'
-
-    id = db.Column(db.Integer, primary_key=True)
-    Name = db.Column(db.Text, nullable=False)
-    Surname = db.Column(db.Text, nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-    def __repr__(self):
-        return '<Message %r>' % self.Name
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -45,14 +30,73 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):
+    article = Article.query.get(id)
+    return render_template("posts_detail.html", article=article)
+
+
+@app.route('/posts/<int:id>/del')
+def posts_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи произошла ошибка"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При добавлении статьи произошла ошибка"
+
+    else:
+        article = Article.query.get(id)
+        return render_template("post_update.html", article=article)
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template("create-article.html")
+
+
 @app.route('/weekend')
 def weekend():
     return render_template("weekend.html")
 
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return 'User page:' + name + "-" + str(id)
-
-if __name__ == '--main__':
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
